8596c845bb52831fd6b48003bec769a6f69aea23
Mouhamed NDOYE <ndoyeahmed2602@gmail.com>
2023-05-26 18:09:16 +0000

    config home components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './content/components/home/home.component';
 
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./content/content.module').then(m => m.ContentModule)
+      }
+    ]
+  },
   {
     path: 'login',
     loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)
