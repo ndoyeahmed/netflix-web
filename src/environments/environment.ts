bb23688140d059a0fd439625ca6f00fb4e480048
Mouhamed NDOYE <49942742+ndoyeahmed@users.noreply.github.com>
2023-06-11 17:41:44 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  url: 'https://net-test.herokuapp.com'
+  production: true,
+  url: 'https://netflix-api.herokuapp.com'
 };
 
 /*
